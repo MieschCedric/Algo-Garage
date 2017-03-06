c3bce44c839520192af15d89ff5b6237a1b32200
DESKTOP-8ALJSBF\Cedri <c.miesch@ludus-academie.com>
2017-03-06 16:09:01 +0100

    Tp Garage

---
+++ garage automobile.pas
@@ -0,0 +1,93 @@
+//	Algorithme Garage automobile
+//	But créer un algorithme sur les garages et les voitures
+//	Entree entrer les voitures et les garages
+//	Sortie	les caractéristiques 
+//
+//	const
+//		garage <- 2
+//		voiture <- 6
+//
+//	variable
+//		i, j : entier
+//
+//	Debut
+//	
+//	Type garage1 = enregistrement
+//		nom : chaîne de caractères
+//		adresse : entier
+//		voie : chaine de caractères
+//		code postale : entier
+//		ville : chaîne de caractères
+//		pays :  chaîne de caractères
+//		n°tel : entier
+//		email : chaine de caractères
+//
+//	FinEnregistrement
+//
+//	Debut
+//		v : tableau de [1..Garage] de garage1
+//		POUR i<- 1 a Garage FAIRE
+//		écrire "quel est le nom du garage ?"
+//		lire (v[i].nom)
+//		écrire "quelle est l'adresse du garage ?"
+//		lire (v[i].adresse)
+//		écrire "quelle est la rue du garage ?"
+//		lire (v[i].voie)
+//		écrire "quel est le code postale du garage ?"
+//		lire (v[i].code postale)
+//		écrire "quel est le nom de la ville du garage ?"
+//		lire (v[i].ville)
+//		écrire "quel est le nom du pays du garage ?"
+//		lire (v[i].pays)
+//		écrire "quel est le numéro de téléphone du garage ?"
+//		lire (v[i].n°tel)
+//		écrire "quelle est l'adresse email du garage ?"
+//		lire (v[i].email)
+//
+// 	Fin
+//
+//	Type marque=(Citroen, Renault, Bmw, Audi, Dacia)
+//
+//	FinEnregistrement
+//
+//	Type énergie=(Essence, Diesel, GPL, Electrique, Hybride)
+//
+//	FinEnregistrement
+//
+//	Type voiture1 = enregistrement
+//		modèle : chaîne de caractères
+//		puissance fiscale : entier
+//		puissance DYN : entier
+//		couleur :  chaîne de caractères
+//		options : chaîne de caractères
+//		année du modèle : entier
+//		prix du modèle : entier
+//		cote argus : entier
+//		date de mise en circulation : entier
+//		age : entier
+//		plaque d'imatriculation : entier
+//
+//	FinEnregistrement
+//
+//	Debut
+//		v2 : tableau de [1..Voiture] de voiture1
+//		POUR j<- 1 a Voiture FAIRE
+//		écrire "quel est le modèle de la voiture?"
+//		lire (v2[j].mdolèle)
+//		écrire "quelle est la puissance fiscale de la voiture?"
+//		lire (v2[j].puissance fiscale)
+//		écrire "quelle est la puissance DYN de la voiture?"
+//		lire (v2[j].puissance DYN)
+//		écrire "quelles sont les options de la voiture?"
+//		lire (v2[j].options)
+//		écrire "quelle est l'année du modèle de la voiture?"
+//		lire (v2[j].année du modèle)
+//		écrire "quelle est la cote argus de la voiture?"
+//		lire (v2[j].cote argus)
+//		écrire "quelle est la date de mise en circulation de la voiture?"
+//		lire (v2[j].date de mise en circulation)
+//		écrire "quel l'age de la voiture?"
+//		lire (v2[j].age)
+//
+// 	Fin
+//
